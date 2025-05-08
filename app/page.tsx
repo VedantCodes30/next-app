4342132447e112fdf2ae95dcc39c5344f4f6530a
Vedant Kale <vedantcodes30@gmail.com>
2025-05-08 19:46:18 +0530

    "fix: some ui changes"

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,17 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import styles from "@/app/ui/home.module.css";
-import { lusitana } from "@/app/ui/fonts";
 import { inter } from "@/app/ui/fonts";
 import Image from "next/image";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape}>
-        <AcmeLogo />
-      </div>
+      <AcmeLogo />
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
-          <p className={`${lusitana.className} antialiased`}>
+          <p className={`${inter.className} antialiased`}>
             <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-600">
               Next.js Learn Course
@@ -36,7 +33,8 @@ export default function Page() {
             width={1000}
             height={760}
             className="hidden md:block"
-            alt="Scrennshots of the dashboard project showing desktop version"
+            alt="Screenshots of the dashboard project showing desktop version"
+            loading="lazy"
           />
 
           {/* Add hero image of mobile */}
